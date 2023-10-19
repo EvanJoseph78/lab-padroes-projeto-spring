b98aa09ac8cd5bae1bdb20b8b06acbd69d95fd6c
evan <evandromariano49@gmail.com>
2023-10-19 10:13:46 -0300

    adicionado tratamento validação

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation.gof.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Cliente {
+
+	/*
+	 * Adicionando validadação
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotBlank(message = "O campo nome é obrigatório")
+	private String nome;
+	@NotNull(message = "O campo endereco não pode ser nulo")
+	@ManyToOne
+	private Endereco endereco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+}
